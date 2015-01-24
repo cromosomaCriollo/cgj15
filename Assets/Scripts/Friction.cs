2053a24790fb5ebb128cb19dc220ef10136ae4f8
Loucry <Loucry@hotmail.com>
2015-01-24 17:30:38 -0430

    Cambio de fricción y agregado de asset
    asset Image Effects

---
+++ Assets/Scripts/Friction.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Friction : MonoBehaviour {
+	public float MaxTime = 2.0f;
+	private float TimerFriction;
+	public float InitialFriction = 3.0f;
+	public float rate = 0.5f;
+	public GameObject self;
+
+	// Use this for initialization
+	void Start () {
+		//transform.Find("Ball").collider2D.sharedMaterial.friction = InitialFriction;
+		self.collider2D.sharedMaterial.friction = InitialFriction;
+		TimerFriction = MaxTime;
+		InitialFriction -= rate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (InitialFriction != 0)
+			TimerFriction -= Time.deltaTime;
+		if (TimerFriction <= 0) {
+			Debug.Log (TimerFriction);
+			if (InitialFriction <= 0.2f) {
+				InitialFriction = 0;
+				self.collider2D.sharedMaterial.friction = InitialFriction;
+			}else{
+				self.collider2D.sharedMaterial.friction = InitialFriction;
+				TimerFriction = MaxTime;
+			}
+			InitialFriction -= rate;
+		}
+
+
+	}
+}
