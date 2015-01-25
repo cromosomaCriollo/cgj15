8693e2699d6f250cf3f8610f70e29476bca0d32b
Atilio Quintero <atilioquintero@gmail.com>
2015-01-25 13:08:49 -0430

    Cambios al game over

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -5,9 +5,6 @@ public class Restart : MonoBehaviour {
 
 	public void OnClick ()
 	{
-		if (Time.timeScale == 0) {
-			Time.timeScale = 1;
-		}
 		Application.LoadLevel (Application.loadedLevelName);
 	}
 
--- Assets/Scripts/VolverMenú.cs
+++ Assets/Scripts/VolverMenú.cs
@@ -3,13 +3,9 @@ using System.Collections;
 
 public class VolverMenú : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public void OnClick ()	{
+		Application.LoadLevel ("MainMenu");
 	}
 }
