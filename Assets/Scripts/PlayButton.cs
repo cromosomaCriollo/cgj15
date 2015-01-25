ca3d6e95259205f2f11fe239b62a3f36f7f76791
Loucry <Loucry@hotmail.com>
2015-01-25 09:42:47 -0430

    Main Menu
    botones y animaciones

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class PlayButton : MonoBehaviour {
 
-	public bool animation_bool = false;
+	//public Animator mainMenuAnimator;
 
 	public void NextScene()
 	{
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+		StartCoroutine ("AnimationPlay");
+	}
+
+	IEnumerator AnimationPlay (){
+		//AnimationInfo[] CurrentClip = mainMenuAnimator.GetNextAnimationClipState (0);
+
+		//yield return new WaitForSeconds (CurrentClip [0].clip.length);
+		yield return new WaitForSeconds (0.45f);
 		Application.LoadLevel ("MainScene");
 	}
 
-	public void PressedDown  ()
-	{
-		if (animation_bool) {
-			animation.Play();
-			NextScene();
-		}
-	}
+
 }
