94fbb03bb391f00dc17727fc49bd4d7292741954
Atilio Quintero <atilioquintero@gmail.com>
2015-01-25 00:45:04 -0430

    Anadiendo mascara para el cronometro

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -15,9 +15,25 @@ public class Timer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		TimerF = TimerF + Time.deltaTime;
-		someText.text = ((int) TimerF).ToString () + " s";
+		someText.text = MascaraMinutos(TimerF);
 		// para pruebas
 		//someText.text = TimerF.ToString ("0.00");
 
+	}
+	public string MascaraMinutos(float tiempo) {
+		string mensaje = "";
+		int minutos = (int)tiempo / 60;
+		int segundos = (int)tiempo - (60 * minutos);
+		if (minutos < 1) {
+				mensaje = segundos + " s";
+		} else {
+				if (segundos > 0) {
+					mensaje = minutos + " m, " + segundos + " s";						
+				} else {
+					mensaje = minutos + " m";	
+				}
+
+		}
+		return  mensaje;				
 	}
 }
