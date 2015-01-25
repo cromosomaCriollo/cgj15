75e25d3c510b83d2d5fa81f44c8d3f2c96fd1517
Loucry <Loucry@hotmail.com>
2015-01-25 12:34:13 -0430

    Scene

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -10,5 +10,7 @@ public class GameOver : MonoBehaviour {
 		HUD.gameObject.SetActive (false);
 		transform.Find("GameOver").gameObject.SetActive(true);
 		transform.Find ("Black").gameObject.SetActive (true);
+		HUD.transform.Find ("VolverMenu").gameObject.SetActive (true);
+		HUD.transform.Find ("Restart").gameObject.SetActive (true);
 	}
 }
--- Assets/Scripts/RandomEvents.cs
+++ Assets/Scripts/RandomEvents.cs
@@ -8,7 +8,6 @@ public class RandomEvents : MonoBehaviour {
 	public Movement target;                                                                                          
 	private bool entro = true;
 	public Transform PrefabWind;
-	private Object WindI;
 	void Start () {
 		CurrentTimer = Timer;
 	}
@@ -24,20 +23,22 @@ public class RandomEvents : MonoBehaviour {
 				float dir;
 				if (Random.Range (0, 2) == 1){
 					target.WindDirection = 'D';
-					dir = 1.00f;
+					dir = -1.00f;
 				}else{
 					target.WindDirection = 'I';
-					dir = -1.00f;
+					dir = 1.00f;
 				}
-				WindI =  Instantiate(PrefabWind, new Vector3 (16.14f, 7.32f, -4.2f), Quaternion.Euler(0.0f, 270.0f, 90.0f));
+				GameObject WindI =  (GameObject) Instantiate(PrefabWind, new Vector3 (14.14f * dir, 7.32f, -4.2f), Quaternion.Euler(0.0f, 270.0f*dir, 90.0f));
+				Destroy(WindI, 2);
 				//instanciar los sprite wind, bla bla
 			}else{ 
 				if (!entro){
 					if (CurrentTimer <= -2.0f){
+
 						target.BlowWind = false;
+
 						CurrentTimer = Timer;
 						entro = true;
-						Destroy(WindI);
 					}
 				}else{
 					CurrentTimer = Timer;
