6c75be23016865d2c198315afde18289a9137ac7
Loucry <Loucry@hotmail.com>
2015-01-25 09:50:58 -0430

    agregar el png de game over

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class PlayButton : MonoBehaviour {
 
-	//public Animator mainMenuAnimator;
 
 	public void NextScene()
 	{
@@ -13,9 +12,6 @@ public class PlayButton : MonoBehaviour {
 	}
 
 	IEnumerator AnimationPlay (){
-		//AnimationInfo[] CurrentClip = mainMenuAnimator.GetNextAnimationClipState (0);
-
-		//yield return new WaitForSeconds (CurrentClip [0].clip.length);
 		yield return new WaitForSeconds (0.45f);
 		Application.LoadLevel ("MainScene");
 	}
